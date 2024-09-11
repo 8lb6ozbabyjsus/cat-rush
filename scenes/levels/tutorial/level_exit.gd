cfc367c12408755756feca4d9e3b7067cc6b6e32
8lb6ozbabyjsus <walkerdalton@yahoo.com>
2024-09-10 22:58:02 -0700

    built a scene manager to handle level transitions

---
+++ scenes/levels/tutorial/level_exit.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@export var next_level : String = "Level_Scoring"
+
+func _on_exit_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		var player = body as CharacterBody2D
+		player.queue_free()
+
+	await get_tree().create_timer(3.0).timeout
+	print("Player entered level exit")
+	SceneManager.load_next_level(next_level)

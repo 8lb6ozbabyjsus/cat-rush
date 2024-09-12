0f524e113fa13b3e92bafe9428020858a2abeafa
8lb6ozbabyjsus <walkerdalton@yahoo.com>
2024-09-11 18:42:10 -0700

    loading screen-scene transition added to manager

--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var loading_screen = preload("res://ui/loading_screen/loading_screen.tscn")
+
 var scenes : Dictionary = {
 	"Tutorial" : "res://scenes/levels/tutorial/tutorial.tscn",
 	# "Level_1" : "res://scenes/levels/level_1/level_1.tscn",
@@ -9,6 +11,10 @@ var scenes : Dictionary = {
 
 func load_next_level(level: String):
 	var scene_path : String = scenes.get(level)
+
 	if scene_path != null:
-		await get_tree().create_timer(1.0).timeout
+		var loading_screen_instance = loading_screen.instantiate()
+		get_tree().root.add_child(loading_screen_instance)
+		await get_tree().create_timer(2.0).timeout
 		get_tree().change_scene_to_file(scene_path)	
+		loading_screen_instance.queue_free()
